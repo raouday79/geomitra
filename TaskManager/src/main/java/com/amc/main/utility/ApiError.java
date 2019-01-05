a5ab84b2d1f2792ce691659f2b53acbf3f76f910
URTECH <URTECH@UR>
2019-01-05 13:56:38 +0530

    third commit

--- TaskManager/src/main/java/com/amc/main/utility/ApiError.java
+++ TaskManager/src/main/java/com/amc/main/utility/ApiError.java
@@ -10,5 +10,40 @@ public class ApiError {
 	private HttpStatus httpStatus;
 	
 	private LocalDateTime localDateTime;
+	
+	private String message;
+	private String debugMessage;
+	public ApiError(HttpStatus httpStatus, LocalDateTime localDateTime, String message, String debugMessage) {
+		super();
+		this.httpStatus = httpStatus;
+		this.localDateTime = localDateTime;
+		this.message = message;
+		this.debugMessage = debugMessage;
+	}
+	public HttpStatus getHttpStatus() {
+		return httpStatus;
+	}
+	public void setHttpStatus(HttpStatus httpStatus) {
+		this.httpStatus = httpStatus;
+	}
+	public LocalDateTime getLocalDateTime() {
+		return localDateTime;
+	}
+	public void setLocalDateTime(LocalDateTime localDateTime) {
+		this.localDateTime = localDateTime;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDebugMessage() {
+		return debugMessage;
+	}
+	public void setDebugMessage(String debugMessage) {
+		this.debugMessage = debugMessage;
+	}
+	
 
 }
---
+++ TaskManager/src/main/java/com/amc/main/utility/RestExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.amc.main.utility;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler
+{
+
+}
